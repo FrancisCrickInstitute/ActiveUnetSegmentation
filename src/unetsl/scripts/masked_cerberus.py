a73892717b17dae346b51f9ba8ac3e1a4994dda9
msmith <matthew.smith3@crick.ac.uk>
2020-05-22 15:16:49 +0100

    Included isotropic scaling before performing the mask prediction.

--- src/unetsl/scripts/masked_cerberus.py
+++ src/unetsl/scripts/masked_cerberus.py
@@ -22,6 +22,38 @@ def guessShaper(key):
     
     return "upsample"
 
+def isotropicScaling(img, new_slices):
+    """
+        multi-channel scaling to go create isotropic scaling by increasing
+        the number of slices.
+        
+        z0   z1  z2  ...zN
+        e0 e1 e2     ...eS
+        Args:
+            img: an image with shape (points, channel,slices, height, width)
+            new_slices: the new number of slices. 
+    """
+    if img.shape[2]==new_slices:
+        #wouldn't change anything.
+        return img
+    #perform a transformation st. e0 == z0 eS==zN
+    slices = img.shape[2]
+    depth = img.shape[2]-1
+    dz = depth/(new_slices-1)
+
+    out = numpy.zeros((img.shape[0], img.shape[1], new_slices, img.shape[3], img.shape[4]), dtype=img.dtype)    
+    for i in range(new_slices):
+        z = dz*i
+        iz = int(z)
+        lo = z - iz
+        if lo==0:
+            out[:, :, i] = img[:, :, iz]
+        else:
+            out[:, :, i] = img[:, :, iz]*(1-lo) + img[:, :, iz+1]*lo
+    
+    return out
+    
+    
 def getCerberusPredictor(model, batch_size=1, gpus=1):
     batch=True #no config.
     
@@ -74,6 +106,14 @@ def getCerberusPredictor(model, batch_size=1, gpus=1):
 
 import time
     
+def getIsotropicFactor(tags):
+    """
+     factor to get x_resolution to equal z resolution.
+     
+    """
+    x = tags['x_resolution']
+    sp = tags['spacing']
+    return x*sp
 
 @click.command()
 @click.argument("masker_file")
@@ -83,6 +123,19 @@ import time
 @click.option("--batch_size", default=2, envvar="BATCH_SIZE")
 @click.option("--chunk_size", default=1, envvar="CHUNK_SIZE")
 def predict(masker_file, cerberus_file, img_file, gpus, batch_size, chunk_size):
+    
+    img_stack, tags = unetsl.data.loadImage(img_file)
+    factor = getIsotropicFactor(tags)
+    
+    new_slices = int(img_stack.shape[2]*factor)
+    print(new_slices, ", ", img_stack.shape[2])
+    factor = new_slices/img_stack.shape[2]
+    
+    if factor != 1:
+        tags['spacing'] = tags['spacing']/factor
+    
+    
+    
     masker_file = pathlib.Path(masker_file)
     img_file = pathlib.Path(img_file)
     cerberus_file = pathlib.Path(cerberus_file)
@@ -90,7 +143,7 @@ def predict(masker_file, cerberus_file, img_file, gpus, batch_size, chunk_size):
     mm = unetsl.masker.MaskerModel( (1, 384, 384, 384) )
     mm.loadModel(masker_file)
         
-    img_stack, tags = unetsl.data.loadImage(img_file)
+    
     
     
     
@@ -101,10 +154,10 @@ def predict(masker_file, cerberus_file, img_file, gpus, batch_size, chunk_size):
     start = time.time()
     print(start, gpus, chunk_size, batch_size)
     for i in range(0, img_stack.shape[0], chunk_size):
-        chunk = img_stack[i:i+chunk_size]
+        chunk = isotropicScaling( img_stack[i:i+chunk_size], new_slices )
         dots = "".join(["."]*chunk.shape[0])
         print(dots, end=" ")
-        pred = mm.predictImages(img_stack[i:i+chunk_size])
+        pred = mm.predictImages(chunk)
         pred2, debug = cerb_predictor.predictImage(pred)
         stacks.append(pred2)
     with open("log.txt", 'a', encoding="UTF8") as fi:
